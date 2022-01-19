951409acb63d42e70cbda77edbdbafd018496709
陈鹏 <cpontheway@gmai.com>
2022-01-20 00:49:25 +0800

    初始化

--- demo-support-main/demo-support-export/src/main/java/com/demo/support/constant/ResultCodeConstant.java
+++
@@ -1,9 +0,0 @@
-package com.demo.support.constant;
-
-public class ResultCodeConstant {
-    public static String SUCCESS="000000"; //成功
-
-    public static String SYSTEM_EXCEPTION="999999"; //成功
-
-
-}
